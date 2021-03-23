e70c15b8072dcec5ea3beebfd904648ec2f18ca4
Kirill <36329442+Geabriel@users.noreply.github.com>
2021-03-23 14:49:22 +0300

    Add files via upload

---
+++ WPF/Day16/RandomUserLibrary/Entities/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace RandomUserLibrary.Entities
+{
+	public class UserInfo
+	{
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public string Foto { get; set; }
+	}
+}
