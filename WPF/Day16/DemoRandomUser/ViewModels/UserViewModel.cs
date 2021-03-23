cc22b35995828213e0a69b5714e4c0b584035bdf
Kirill <36329442+Geabriel@users.noreply.github.com>
2021-03-23 14:22:49 +0300

    Add files via upload

---
+++ WPF/Day16/DemoRandomUser/ViewModels/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using DemoRandomUser.Model.Repositories;
+using RandomUserLibrary.Entities;
+
+namespace DemoRandomUser.ViewModels
+{
+	public class UserViewModel
+	{
+		private readonly UserRepository _userRepository;
+
+		public UserViewModel()
+		{
+			_userRepository=new UserRepository();
+		}
+		private ObservableCollection<UserInfo> _users;
+
+		public ObservableCollection<UserInfo> Users
+		{
+			get
+			{
+				if (_users == null)
+				{
+					_users = _userRepository.Users;
+				}
+				return _users;
+			}
+		}
+
+
+	}
+}
