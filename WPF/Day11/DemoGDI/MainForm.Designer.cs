6cfd2e2bf25790ca8a587c0a9592bb4bb66c18b8
Kirill <36329442+Geabriel@users.noreply.github.com>
2021-03-23 14:16:19 +0300

    Add files via upload

---
+++ WPF/Day11/DemoGDI/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DemoGDI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDrowRec = new System.Windows.Forms.Button();
+            this.btnDrowImege = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDrowRec
+            // 
+            this.btnDrowRec.Location = new System.Drawing.Point(38, 217);
+            this.btnDrowRec.Name = "btnDrowRec";
+            this.btnDrowRec.Size = new System.Drawing.Size(131, 58);
+            this.btnDrowRec.TabIndex = 0;
+            this.btnDrowRec.Text = "DrowRectangle";
+            this.btnDrowRec.UseVisualStyleBackColor = true;
+            this.btnDrowRec.Click += new System.EventHandler(this.btnDrowRec_Click);
+            // 
+            // btnDrowImege
+            // 
+            this.btnDrowImege.Location = new System.Drawing.Point(213, 217);
+            this.btnDrowImege.Name = "btnDrowImege";
+            this.btnDrowImege.Size = new System.Drawing.Size(131, 58);
+            this.btnDrowImege.TabIndex = 1;
+            this.btnDrowImege.Text = "DrowImege";
+            this.btnDrowImege.UseVisualStyleBackColor = true;
+            this.btnDrowImege.Click += new System.EventHandler(this.btnDrowImege_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 528);
+            this.Controls.Add(this.btnDrowImege);
+            this.Controls.Add(this.btnDrowRec);
+            this.Name = "MainForm";
+            this.Text = "DemoGDI";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDrowRec;
+        private System.Windows.Forms.Button btnDrowImege;
+    }
+}
+
---
+++ WPF/Day11/DemoGDI/MainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoGDI
+{
+    public partial class MainForm : Form
+    {
+        private int countPaint;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            //1
+            Graphics ghr = e.Graphics;
+            Pen pen = new Pen(Color.Red, 5);
+            ghr.DrawRectangle(pen,50,50,150,150);
+            countPaint++;
+            this.Text = countPaint.ToString();
+            SolidBrush solidBrush = new SolidBrush(Color.Green);
+            TextureBrush textureBrush = new TextureBrush(Image.FromFile("dog.jpg"));
+            HatchBrush hatchBrush = new HatchBrush(HatchStyle.Cross, Color.OrangeRed);
+            LinearGradientBrush linearGradientBrush 
+                = new LinearGradientBrush(new Point(10,0), new Point(200,0), Color.Red, Color.Green);
+
+            ghr.FillRectangle(linearGradientBrush, 50, 50, 150, 150);
+            ghr.DrawString("C# .NET", new Font("Arial", 50), linearGradientBrush, 350, 350);
+        }
+
+        private void btnDrowRec_Click(object sender, EventArgs e)
+        {
+            //2
+            Graphics grh = this.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 5);
+            grh.DrawRectangle(pen, 250, 250, 150, 150);
+            countPaint++;
+            this.Text = countPaint.ToString();
+            
+            //2.1
+            //btnDrowRec.CreateGraphics();
+        }
+
+        private void btnDrowImege_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap("dog.jpg");
+            Graphics grh = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Red, 5);
+            grh.DrawEllipse(pen, 250, 250, 150, 150);
+            bmp.Save("New_dog.jpg");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //GraphicsPath path = new GraphicsPath();
+            //path.AddEllipse(new Rectangle(50, 50, 150, 150));
+            //path.AddRectangle(new Rectangle(200, 50, 150, 150));
+            //Region regionForm = new Region(path);
+            //this.Region = regionForm;
+        }
+    }
+}
