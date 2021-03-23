9950479b94d3813d28b9e7c9d6fc71255098a798
Kirill <36329442+Geabriel@users.noreply.github.com>
2021-03-23 14:42:24 +0300

    Add files via upload

---
+++ WPF/Day11/PanalControls/RotateManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PanalControls
+{
+    public class RotateManager : FrameworkElement
+    {
+        public static double GetAngle(DependencyObject obj)
+        {
+            return (double)obj.GetValue(AngleProperty);
+        }
+
+        public static void SetAngle(DependencyObject obj, double value)
+        {
+            obj.SetValue(AngleProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.RegisterAttached(
+                "Angle",
+                typeof(double),
+                typeof(UserControl1),
+                new PropertyMetadata(0.0) {PropertyChangedCallback = OnAngleChanged });
+
+        private static void OnAngleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var element = d as UIElement;
+            if(element != null)
+            {
+                element.RenderTransformOrigin = new Point(0.5, 0.5);
+                element.RenderTransform = new RotateTransform((double)e.NewValue);
+            }
+        }
+    }
+}
