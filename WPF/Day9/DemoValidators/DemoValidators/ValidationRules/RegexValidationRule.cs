35a4f4653355435bfb4028088acb1a060499c874
Kirill <36329442+Geabriel@users.noreply.github.com>
2021-03-23 14:27:01 +0300

    Add files via upload

---
+++ WPF/Day9/DemoValidators/DemoValidators/ValidationRules/RegexValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DemoValidators.ValidationRules
+{
+    public class RegexValidationRule : ValidationRule
+    {
+        public string Pattern { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!String.IsNullOrWhiteSpace(Pattern))
+            {
+                if (Regex.IsMatch(value.ToString(), Pattern))
+                {
+                    return new ValidationResult(true, null);
+                }
+                else
+                {
+                    return new ValidationResult(false, "IMEI is no correct");
+                }
+            }
+            return new ValidationResult(false, "Pattern is empty");
+        }
+    }
+}
